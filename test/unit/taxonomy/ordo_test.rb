83d32a41432a05783857f4c4c84b89692e5fa013
Vitalii Khustochka <xyctka@gmail.com>
2009-11-10 00:54:28 +0200

    taxonomy models moved into subdir

--- test/unit/taxonomy/ordo_test.rb
+++ test/unit/taxonomy/ordo_test.rb
@@ -3,10 +3,11 @@ require 'test_helper'
 class OrdoTest < ActiveSupport::TestCase
 
   test "insert should preserve sorting" do
+    new_sort = Ordo.count / 2
     assert_difference("Ordo.count", 1) do
-      Ordo.new(:name_la => "Midiformes", :name_ru => "Среднеобразные", :name_uk => "Середньоподібні", :description => '', :sort => Ordo.count / 2).insert_mind_sorting
+      Ordo.new(:name_la => "Midiformes", :name_ru => "Среднеобразные", :name_uk => "Середньоподібні", :description => '', :sort => new_sort).insert_mind_sorting
     end
-    assert_equal Ordo.find_by_name_la("Midiformes")[:sort], Ordo.count / 2, "Badly inserted"
+    assert_equal Ordo.find_by_name_la("Midiformes")[:sort], new_sort, "Badly inserted"
     assert_sorting_preserved(Ordo)
   end
 
@@ -18,18 +19,20 @@ class OrdoTest < ActiveSupport::TestCase
   end
 
   test "move down should preserve sorting" do
+    new_sort = Ordo.count * 2 / 3
     for_update = Ordo.find_by_sort(Ordo.count / 3)
-    for_update.update_mind_sorting(:sort => Ordo.count * 2 / 3)
+    for_update.update_mind_sorting(:sort => new_sort)
 
-    assert_equal Ordo.count * 2 / 3, Ordo.find_by_name_la(for_update[:name_la])[:sort], "Badly inserted"
+    assert_equal new_sort, Ordo.find_by_name_la(for_update[:name_la])[:sort], "Badly inserted"
     assert_sorting_preserved(Ordo)
   end
 
   test "move up should preserve sorting" do
+    new_sort = Ordo.count / 3
     for_update = Ordo.find_by_sort(Ordo.count * 2 / 3)
-    for_update.update_mind_sorting(:sort => Ordo.count / 3)
+    for_update.update_mind_sorting(:sort => new_sort)
 
-    assert_equal Ordo.count / 3, Ordo.find_by_name_la(for_update[:name_la])[:sort], "Badly inserted"
+    assert_equal new_sort, Ordo.find_by_name_la(for_update[:name_la])[:sort], "Badly inserted"
     assert_sorting_preserved(Ordo)
   end
 
