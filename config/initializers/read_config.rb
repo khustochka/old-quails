9b0ee05c8a657da307696ab3487d3acf0e01daad
Vitalii Khustochka <xyctka@gmail.com>
2009-11-12 00:06:23 +0200

    fix typos

--- config/initializers/read_config.rb
+++ config/initializers/read_config.rb
@@ -2,5 +2,5 @@ require 'yaml'
 
 # read config from yml and merge with ENV (for heroku)
 configs  = (YAML.load_file('config/config.yml')[RAILS_ENV] rescue {})
-configs = configs.merge((YAML.load_file('admin_config/config.yml')[RAILS_ENV] rescue {}))
+configs = configs.merge((YAML.load_file('config/admin_config.yml')[RAILS_ENV] rescue {}))
 CONFIG = configs.merge(ENV).symbolize_keys
