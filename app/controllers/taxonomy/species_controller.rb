a1a2561f15f4cb3101f13728f06633713fd549d9
Vitalii Khustochka <xyctka@gmail.com>
2009-12-06 02:22:00 +0200

    public layout fixed and used for species index

--- app/controllers/taxonomy/species_controller.rb
+++ app/controllers/taxonomy/species_controller.rb
@@ -1,2 +1,8 @@
 class SpeciesController < TaxaController
+  def index
+    respond_to do |format|
+      format.html { render 'taxa/index', :layout => "public" }
+      #format.xml  { render :xml => @taxa }
+    end
+  end
 end
