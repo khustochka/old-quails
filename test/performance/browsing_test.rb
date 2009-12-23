6bc2a3b4a1f23ca1b15cca50ce2436a90ca1fe5d
Vitalii Khustochka <xyctka@gmail.com>
2009-12-23 14:17:17 +0200

    updated fixtures; created real performance test (species list)

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -3,7 +3,12 @@ require 'performance_test_help'
 
 # Profiling results for each test method are written to tmp/performance.
 class BrowsingTest < ActionController::PerformanceTest
-  def test_homepage
-    get '/'
+#  def test_homepage
+#    get '/'
+#  end
+
+  def test_species_list
+    get '/aves'
   end
+
 end
